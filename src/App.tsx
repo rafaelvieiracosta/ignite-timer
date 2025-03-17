6970234fd118ab8fdd692a7316a4e0a7e71e3803
rafaelvieiracosta <rvc.vieira@outlook.com>
2025-03-16 22:39:22 -0300

    feat: add fonts, global css and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
-  return <h1>ignite-timer</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>ignite-timer</h1>;
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
