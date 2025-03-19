14b73aa8ae2e35b2a4c8007975bce9ae671594c5
rafaelvieiracosta <rvc.vieira@outlook.com>
2025-03-19 19:08:42 -0300

    chore: add styles components dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from "styled-components";
 
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
